4e5888d15c704b84e71fb4c4835066b92714f724
akash <akash.ry@somaiya.edu>
2021-03-01 21:31:43 +0530

    frontend complete

---
+++ script.js
@@ -0,0 +1,65 @@
+
+const containEvents = document.querySelector('.events-container')
+const addEventsPopup = document.querySelector('.add-events-popup')
+const closePopup = document.querySelector('.close')
+const addBtn = document.querySelector('#add-btn')
+
+addBtn.addEventListener('click',()=>{
+  addEventsPopup.classList.add('active')
+})
+closePopup.addEventListener('click',()=>{
+  addEventsPopup.classList.remove('active')
+})
+
+API_URL = "/.netlify/functions/server/api"
+// get request
+fetch(API_URL)
+  .then(async (res) => {
+    // console.log(res.json())
+    const events =await res.json()
+    console.log(events)
+    events.forEach(event =>{
+      const divParent = document.createElement('div')
+      const msg = event.msg? event.msg : 'no description'
+      const newElement = `<div>Event Name: ${event.name}</div>
+      <div>Date: ${event.date.slice(0,10)}</div>
+      <div>Timing: ${event.time}</div>
+      <div>More info: ${msg}</div>
+      <div style="margin-top:20px;"><a class="register-btns btn" href=${event.registrationLink}>Register Now</a></div>
+      <br>
+      `
+      divParent.innerHTML = newElement
+      
+      const mainContainer = document.createElement('div')
+      mainContainer.classList.add('event-card')
+      mainContainer.innerHTML = divParent.innerHTML
+      containEvents.append(mainContainer) 
+
+    })
+
+  })
+  .catch(err => console.log(err))
+
+
+
+const submitBtn = document.querySelector('#btn-submit')
+const form = document.querySelector('form')
+submitBtn.addEventListener('click',()=>{
+  //POST REQUEST 
+  formd = new FormData(form);
+  fetch(API_URL,{
+    method: 'POST',
+    body: JSON.stringify({
+      name: formd.get("name"),
+      date: formd.get("date"),
+      time: formd.get("time"),
+      registrationLink: formd.get("link"),
+      msg: formd.get("msg")
+    }),
+    headers:{
+      'Content-type': 'application/x-www-form-urlencoded; charset=utf-8'
+    }
+  })
+})
+
+
